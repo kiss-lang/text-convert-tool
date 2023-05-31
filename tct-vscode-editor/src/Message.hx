dcca2dc97e34c3d6275c5e6312a806ed381b64cc
Nat Quayle Nelson <natquaylenelson@gmail.com>
2023-05-31 13:52:30 -0600

    onSuccess mechanism for ui updates

--- tct-vscode-editor/src/Message.hx
+++ tct-vscode-editor/src/Message.hx
@@ -1,6 +1,8 @@
 import tct.FileConversionProject;
 
 enum Message {
+    RequestSuccess;
+    RequestFailure;
     RequestDelete(blockIndex:Int);
     Initialize(blocks:Array<Block>);
     Print(message:String);
