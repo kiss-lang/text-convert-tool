8d0362fb717c5e19988049d96c52145390230359
Nat Quayle Nelson <natquaylenelson@gmail.com>
2023-05-31 13:52:43 -0600

    locking/unlocking with checkbox

--- tct-vscode-editor/src/Message.hx
+++ tct-vscode-editor/src/Message.hx
@@ -4,6 +4,7 @@ enum Message {
     RequestSuccess;
     RequestFailure;
     RequestDelete(blockIndex:Int);
+    RequestToggleLock(blockIndex:Int);
     Initialize(blocks:Array<Block>);
     Print(message:String);
     Error(message:String);
